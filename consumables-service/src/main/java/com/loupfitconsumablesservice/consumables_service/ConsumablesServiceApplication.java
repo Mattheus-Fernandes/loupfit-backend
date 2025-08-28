b39d725376ef504a9231fbabaf2d4fd0c74c933e
Mattheus Fernandes <mattheus.ale@hotmail.com>
2025-08-28 12:46:11 -0300

    commit inicial

---
+++ consumables-service/src/main/java/com/loupfitconsumablesservice/consumables_service/ConsumablesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.loupfitconsumablesservice.consumables_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumablesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumablesServiceApplication.class, args);
+	}
+
+}
---
+++ consumables-service/src/test/java/com/loupfitconsumablesservice/consumables_service/ConsumablesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loupfitconsumablesservice.consumables_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumablesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
