046e82bdf8b94ad3317703ff7499a4e29f2602b6
Mattheus Fernandes <mattheus.ale@hotmail.com>
2025-09-18 17:35:11 -0300

    feat(asset-service): adicionado metodo get para buscar de todos equipamento e por usuario

--- asset-service/src/main/java/com/loupfitassetservice/asset_service/business/AssetService.java
+++ asset-service/src/main/java/com/loupfitassetservice/asset_service/business/AssetService.java
@@ -1,15 +1,18 @@
 package com.loupfitassetservice.asset_service.business;
 
 import com.loupfitassetservice.asset_service.business.dto.AssetDTO;
+import com.loupfitassetservice.asset_service.business.dto.UserDTO;
 import com.loupfitassetservice.asset_service.business.mapper.AssetConverter;
+import com.loupfitassetservice.asset_service.infrastructure.client.UserClient;
 import com.loupfitassetservice.asset_service.infrastructure.entity.Asset;
 import com.loupfitassetservice.asset_service.infrastructure.exceptions.ConflictExcpetion;
 import com.loupfitassetservice.asset_service.infrastructure.repository.AssetRepository;
-import com.loupfitassetservice.asset_service.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,15 +20,27 @@ public class AssetService {
 
     private final AssetRepository assetRepository;
     private final AssetConverter assetConverter;
-    private final JwtUtil jwtUtil;
+    private final UserClient userClient;
 
-    public AssetDTO addAsset(AssetDTO assetDTO) {
+    private UserDTO getCurrentUser(String token) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null) {
+            throw new ConflictExcpetion("Usuário não autenticado");
+        }
+
         String username = auth.getName();
+
+        return userClient.findUserByUsername(token, username);
+
+    }
+
+    public AssetDTO addAsset(String token, AssetDTO assetDTO) {
+        UserDTO userDTO = getCurrentUser(token);
 
         existAsset(assetDTO.getAssetName());
 
-        assetDTO.setCreatedBy(username);
+        assetDTO.setCreatedBy(userDTO.getUsername());
 
         Asset assetEntity = assetConverter.assetEntity(assetDTO);
 
@@ -44,6 +59,14 @@ public class AssetService {
         } catch (ConflictExcpetion e) {
             throw new ConflictExcpetion(e.getMessage());
         }
+    }
+
+    public List<AssetDTO> filterAllAssets() {
+       return assetConverter.assetDTOList(assetRepository.findAll());
+    }
+
+    public List<AssetDTO> assetAddedByCreatedBy(String username) {
+        return assetConverter.assetDTOList(assetRepository.findByCreatedBy(username));
     }
 
 }
--- asset-service/src/main/java/com/loupfitassetservice/asset_service/controller/AssetController.java
+++ asset-service/src/main/java/com/loupfitassetservice/asset_service/controller/AssetController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/asset")
 @RequiredArgsConstructor
@@ -14,7 +16,17 @@ public class AssetController {
     private final AssetService assetService;
 
     @PostMapping
-    public ResponseEntity<AssetDTO> saveAsset(@RequestBody AssetDTO assetDTO) {
-        return ResponseEntity.ok(assetService.addAsset(assetDTO));
+    public ResponseEntity<AssetDTO> saveAsset(@RequestHeader("Authorization") String token, @RequestBody AssetDTO assetDTO) {
+        return ResponseEntity.ok(assetService.addAsset(token, assetDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AssetDTO>> findAllAssets() {
+        return ResponseEntity.ok(assetService.filterAllAssets());
+    }
+
+    @GetMapping("/by-username")
+    public ResponseEntity<List<AssetDTO>> findAssetByUsername(@RequestParam String username) {
+        return ResponseEntity.ok(assetService.assetAddedByCreatedBy(username));
     }
 }
