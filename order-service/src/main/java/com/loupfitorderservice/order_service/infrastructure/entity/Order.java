34b49394fe252809570d0ebd089214bf4ab38d6a
Mattheus Fernandes <mattheus.ale@hotmail.com>
2025-10-18 12:27:12 -0300

    feat(order-service): adicionado o campo imageUrl em entity e OrderDTO

--- order-service/src/main/java/com/loupfitorderservice/order_service/infrastructure/entity/Order.java
+++ order-service/src/main/java/com/loupfitorderservice/order_service/infrastructure/entity/Order.java
@@ -19,6 +19,7 @@ public class Order {
     private String orderId;
     private Long productId;
     private String productName;
+    private String imageUrl;
     private Integer quantity;
     private BigDecimal totalPrice;
     private String size;
