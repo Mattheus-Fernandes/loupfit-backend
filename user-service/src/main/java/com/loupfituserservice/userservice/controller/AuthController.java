1295f06d5adba179e26ed204700b1185b9cb78fc
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-01-22 19:15:49 -0300

    refactor(user-service): melhoria no metodo de fazer login

--- user-service/src/main/java/com/loupfituserservice/userservice/controller/AuthController.java
+++ user-service/src/main/java/com/loupfituserservice/userservice/controller/AuthController.java
@@ -1,15 +1,13 @@
 package com.loupfituserservice.userservice.controller;
 
-import com.loupfituserservice.userservice.business.record.LoginRequest;
+import com.loupfituserservice.userservice.business.record.login.in.LoginRequest;
+import com.loupfituserservice.userservice.business.record.login.out.LoginResponse;
 import com.loupfituserservice.userservice.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,21 +22,19 @@ public class AuthController {
     private final JwtUtil jwtUtil;
 
     @PostMapping
-    public ResponseEntity<String> doLogin(@RequestBody LoginRequest request) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(request.username(), request.password())
-            );
+    public ResponseEntity<LoginResponse> doLogin(@RequestBody LoginRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.username(),
+                        request.password()
+                )
+        );
 
-            String token = jwtUtil.generateToken(authentication.getName());
+        String token = jwtUtil.generateToken(authentication.getName());
 
-            return ResponseEntity.ok(token);
-
-        } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Senha incorreta");
-        } catch (UsernameNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
-        }
+        return ResponseEntity.ok(
+                new LoginResponse(token, "Bearer")
+        );
     }
 }
 
